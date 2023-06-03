2d2066c18df4cd7023f1c0bc5d02b19e8c3a936b
Eduardo Riboli <EduardoRiboli71@gmail.com>
2023-06-03 11:36:04 -0300

    adicionado comentarios

--- Bfs.java
+++ Bfs.java
@@ -18,27 +18,34 @@ public class Bfs {
  
     public static void main(String[] args) {
         try {
+            // lê o arquivo
             BufferedReader reader = new BufferedReader(new FileReader("casos-cohen/mapa2000.txt"));
             String line = reader.readLine();
             String[] values = line.split(" ");
+            // separa a primeira linha entre linha e coluna e armazena os valores.
             int rows = allRows = Integer.parseInt(values[0]);
             int cols = allColumn = Integer.parseInt(values[1]);
 
+            // cria uma matriz de vértices
             Vertex[][] vertexs = new Vertex[rows][cols];
             
+            // para cada linha, pega sua coluna e cria um vértice com sua posição e valor
             for (int i = 0; i < rows; i++) {
                 line = reader.readLine();
                 for (int j = 0; j < cols; j++) {
                     char value = line.charAt(j);
                     vertexs[i][j] = new Vertex(i, j, value);
-                    if (value == '1') initialNumber = new int[]{i, j}; 
+                    if (value == '1') initialNumber = new int[]{i, j}; // se o numero for 1, armazena sua coordenada
                 }
             }
 
+            // cria uma auxiliar de vertices.
             allVertexs = vertexs;
 
             reader.close();
 
+            // para cada linha e coluna da matrix, verifica seus adjascentes,
+            // se for válido e diferente de *, adiciona como sua aresta.
             for (int i = 0; i < rows; i++) {
                 for (int j = 0; j < cols; j++) {
                     Vertex current = vertexs[i][j];
@@ -73,6 +80,7 @@ public class Bfs {
         }
     }
 
+    // método utilizado para voltar ao elemento pai depois de encontrar o elemento que deseja.
     private static void recursiveParent(Vertex vertex) {
         if (vertex.parent == null){
             System.out.println("Viajando de " + Character.getNumericValue(vertex.value) + " para " + (number - 1) + " foi gasto um total de combustível: " + fuel);
@@ -86,6 +94,7 @@ public class Bfs {
         }
     }
 
+    // método para resetar todos os visitados da matriz.
     private static void resetVisited(){
         for (int i = 0; i < allRows; i++) {
             for (int j = 0; j < allColumn; j++) {
@@ -96,29 +105,36 @@ public class Bfs {
     }
 
     private static void bfs(Vertex[][] vertex, int[] start) {
+        // retira o primeiro elemento da fila.
         Queue<int[]> queue = new ArrayDeque<>();
         queue.offer(start);
 
+        // enquanto a fila não estiver vazia, faça:
         while (!queue.isEmpty()) {
             int[] curr = queue.poll();
             int x = curr[0];
             int y = curr[1];
         
+            // marca como visitado
             vertex[x][y].visited = true;
 
+            // se o elemento da matriz for igual ao número que queremos chegar:
             if (Character.getNumericValue(vertex[x][y].value) == number) {
-                number++;
-                recursiveParent(vertex[x][y]);
+                number++; // incrementa a variavel que serve para acharmos os numeros;
+                recursiveParent(vertex[x][y]); // verifica a quantidade de combustivel gasta;
+                // se o numero for igual a 2, acabou o programa.
                 if (number == 2) {
                     System.out.println("Total de combustível: " + sumFuel); 
                     break;
                 }
-                vertex[x][y].parent = null;
-                value = Character.getNumericValue(vertex[x][y].value);
-                coordinateLastNumber = new int[]{x, y};
-                resetVisited();
-                queue.clear();
+                vertex[x][y].parent = null; // coloca o elemento atual com o pai == null;
+                value = Character.getNumericValue(vertex[x][y].value); // armazena o valor dele
+                coordinateLastNumber = new int[]{x, y}; // armazena a coordenada dele
+                resetVisited(); // reseta os visitados
+                queue.clear(); // reseta a fila
 
+                // caso o numero seja maior que 9, pega-se as coordenadas do primeiro elemento (1)
+                // e realiza a bfs para encontrá-lo a partir do último elemento da matriz.
                 if (number > 9) {
                     int newX = initialNumber[0];
                     int newY = initialNumber[1];
@@ -129,6 +145,7 @@ public class Bfs {
                 }
             }
 
+            // para cada aresta desse vértice, adiciona na lista.
             for (Edge edge : vertex[x][y].edges) {
                 Vertex edges = edge.to;
         
@@ -143,6 +160,8 @@ public class Bfs {
                 }
             }
 
+            // se a fila estiver vazia, significa que não foi possível encontrar o elemento
+            // ou seja, deve chamar de novo a bfs a partir do ultimo elemento encontrado.
             if (queue.isEmpty() && number != 10){
                 if (number == 2) {
                     if (sumFuel != 0){
